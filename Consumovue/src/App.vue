156272560989f67e8dcce98d4d8b4e290c95299b
Frans Sebastian Villamizar Maldonado <fsvillamizar@uniboyaca.edu.co>
2023-10-03 16:29:50 -0500

    Diseño y función

--- Consumovue/src/App.vue
+++ Consumovue/src/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
-    <h1>SI QUIERES IR A LA SIGUIENTE SECCIÓN DALE CLIC AL BOTÓN :D</h1>
-    <button @click="openNewTab">Ir a la siguiente sección</button>
+  <div class="container">
+    <div class="center-vertically">
+      <h1 class="title">SI QUIERES IR A LA SIGUIENTE SECCIÓN DALE CLIC AL BOTÓN :D</h1>
+      <button class="button" @click="openNewTab">Ir a la siguiente sección</button>
+    </div>
   </div>
 </template>
 
@@ -24,3 +26,41 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+
+.center-vertically {
+  text-align: center;
+}
+
+.title {
+  font-size: 2rem;
+  margin-bottom: 20px;
+}
+
+.button {
+  padding: 1rem 1rem;
+  background-color: #ff0000;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 18px;
+  transition: background-color 0.3s;
+}
+
+.button:hover {
+  background-color: #0056b3;
+}
+
+/* Estilo para cuando se hace clic en el botón */
+.button:active {
+  background-color: #004399;
+}
+</style>
