b7936dec88784ce8ff1b996c791a88a6cc417c11
Frans Sebastian Villamizar Maldonado <fsvillamizar@uniboyaca.edu.co>
2023-10-03 16:29:02 -0500

    Inicio sencillo

---
+++ Consumovue/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>SI QUIERES IR A LA SIGUIENTE SECCIÓN DALE CLIC AL BOTÓN :D</h1>
+    <button @click="openNewTab">Ir a la siguiente sección</button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  name: 'App',
+  setup() {
+    const nextPageUrl = ref('/PagCarts'); // Cambia '/PagCarts' a la URL deseada
+
+    const openNewTab = () => {
+      window.open(nextPageUrl.value, '_blank');
+    };
+
+    return {
+      nextPageUrl,
+      openNewTab,
+    };
+  },
+}
+</script>
